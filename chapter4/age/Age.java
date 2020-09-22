ddb6a16a02e37af105329ed4edcc359cc13c27ff
sabrina-brooklyness <intern_cs_1@brooklyness.com>
2020-09-22 17:58:41 -0400

    Age

---
+++ chapter4/age/Age.java
@@ -0,0 +1,24 @@
+package chapter4.age;
+
+import java.util.Scanner;
+
+public class Age {
+    public static void main(String[] args){
+        final int MINOR = 21;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        int age = scan.nextInt();
+
+        System.out.println("You entered: " + age);
+        
+        if( age < MINOR){
+            System.out.println("Youth is a wonderful thing. Enjoy.");
+        }
+
+        System.out.println("Age is a state of mind.");
+
+        scan.close();
+    }
+}
